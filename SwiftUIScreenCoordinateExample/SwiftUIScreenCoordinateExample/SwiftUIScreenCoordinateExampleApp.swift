9befc92299172c8173d78ba1efdacd5d81f831c3
znak99 <xhahekcldhkznak0113@gmail.com>
2024-06-09 16:23:56 +0900

    Initial commit

---
+++ SwiftUIScreenCoordinateExample/SwiftUIScreenCoordinateExample/SwiftUIScreenCoordinateExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIScreenCoordinateExampleApp.swift
+//  SwiftUIScreenCoordinateExample
+//
+//  Created by seungwoo on 2024/06/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIScreenCoordinateExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
